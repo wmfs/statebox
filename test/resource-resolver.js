d677c58fa5feb8439c7fa79c9ecb89769b7ad3bb
Jez Higgins <jez@jezuk.co.uk>
2019-02-08 10:05:34 +0000

    fix: Tightened up resource resolver error message

--- test/resource-resolver.js
+++ test/resource-resolver.js
@@ -6,14 +6,19 @@ const expect = chai.expect
 const Resources = require('../lib/resources')
 
 describe('Resource Resolver', () => {
-  it('Throw when no resource specified', () => {
-    for (const p of [undefined, null, '']) {
+  for (const p of [undefined, null, '']) {
+    it(`Throw when no resource specified - '${p}'`, () => {
       expect(() => Resources.resolve(p)).to.throw('No \'Resource\' property set?')
-    }
-  })
-  it('Throw with invalid resource name', () => {
-    for (const p of ['fred', 'fred:', ':fred', '     ', 'fred:  pants']) {
-      expect(() => Resources.resolve(p)).to.throw(`Invalid resource identifier: ${p}`)
-    }
-  })
+    })
+  }
+  for (const p of ['fred', 'fred:', 'fred:  pants']) {
+    it(`Throw with invalid resource name - ${p}`, () => {
+      expect(() => Resources.resolve(p)).to.throw(`Unknown resource type 'fred' in '${p}'`)
+    })
+  }
+  for (const p of [':fred', '     ']) {
+    it(`Throw with invalid resource name - ${p}`, () => {
+      expect(() => Resources.resolve(p)).to.throw(`Unknown resource type '' in '${p}'`)
+    })
+  }
 })
