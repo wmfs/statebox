f1419a2399d4331eb5fbd0982c959f3bd09035d3
Jez Higgins <jez@jezuk.co.uk>
2020-10-28 16:37:35 +0000

    fix(State.Format): more wrong number of arguments tests

--- test/intrinsic-functions.js
+++ test/intrinsic-functions.js
@@ -7,29 +7,43 @@ const expect = chai.expect
 const intrinsics = require('../lib/state-machines/instrinsics')
 
 describe('States.Format', () => {
-  const goodFormatTests = [
-    [['test'], 'test'],
-    [['insert ->{}<- here', 'word'], 'insert ->word<- here'],
-    [['insert ->{}<- here', true], 'insert ->true<- here'],
-    [['insert ->{}<- here', 1], 'insert ->1<- here'],
-    [['insert ->{}<- here', 1452.1212], 'insert ->1452.1212<- here'],
-    [['insert ->{}<- here', null], 'insert ->null<- here'],
-    [['{}, {}, {}', 'word', 100, true], 'word, 100, true'],
-    [['{}<-at start', 'here'], 'here<-at start'],
-    [['at end->{}', 'here'], 'at end->here'],
-    [['{}', null], 'null'],
-    [['{}{}', null, null], 'nullnull']
-  ]
+  describe('good arguments', () => {
+    const goodFormatTests = [
+      [['test'], 'test'],
+      [['insert ->{}<- here', 'word'], 'insert ->word<- here'],
+      [['insert ->{}<- here', true], 'insert ->true<- here'],
+      [['insert ->{}<- here', 1], 'insert ->1<- here'],
+      [['insert ->{}<- here', 1452.1212], 'insert ->1452.1212<- here'],
+      [['insert ->{}<- here', null], 'insert ->null<- here'],
+      [['{}, {}, {}', 'word', 100, true], 'word, 100, true'],
+      [['{}<-at start', 'here'], 'here<-at start'],
+      [['at end->{}', 'here'], 'at end->here'],
+      [['{}', null], 'null'],
+      [['{}{}', null, null], 'nullnull']
+    ]
 
-  for (const [args, expected] of goodFormatTests) {
-    it(`format(${args.map(a => '"' + a + '"').join(', ')})`, () => {
-      const result = intrinsics.format(...args)
-      expect(result).to.equal(expected)
-    })
-  }
+    for (const [args, expected] of goodFormatTests) {
+      it(`format(${args.map(a => '"' + a + '"').join(', ')})`, () => {
+        const result = intrinsics.format(...args)
+        expect(result).to.equal(expected)
+      })
+    }
+  })
 
-  it('format("test", "extra", "arguments") fails', () => {
-    const test = () => intrinsics.format('test', 'extra', 'arguments')
-    expect(test, "format('test', 'extra', 'arguments') should throw").to.throw()
+  describe('malformed arguments', () => {
+    const badFormatTests = [
+      ['test', 'extra', 'arguments'],
+      ['test {}', 'yes', 'oh dear'],
+      ['too few args {}'],
+      ['still too few {} {} {}', 1, 2]
+    ]
+
+    for (const args of badFormatTests) {
+      const asString = `format(${args.map(a => '"' + a + '"').join(', ')})`
+      it(asString, () => {
+        const test = () => intrinsics.format(args)
+        expect(test, `${asString} should throw`).to.throw()
+      })
+    }
   })
 })
