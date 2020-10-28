bc9e68f5b0c3c2710d123672128bf52ea6b7045d
Jez Higgins <jez@jezuk.co.uk>
2020-10-28 16:19:10 +0000

    fix(States.Format): validate number of arguments

--- test/intrinsic-functions.js
+++ test/intrinsic-functions.js
@@ -7,7 +7,7 @@ const expect = chai.expect
 const intrinsics = require('../lib/state-machines/instrinsics')
 
 describe('States.Format', () => {
-  const formatTests = [
+  const goodFormatTests = [
     [['test'], 'test'],
     [['insert ->{}<- here', 'word'], 'insert ->word<- here'],
     [['insert ->{}<- here', true], 'insert ->true<- here'],
@@ -16,15 +16,20 @@ describe('States.Format', () => {
     [['insert ->{}<- here', null], 'insert ->null<- here'],
     [['{}, {}, {}', 'word', 100, true], 'word, 100, true'],
     [['{}<-at start', 'here'], 'here<-at start'],
-    [['at end->{}','here'], 'at end->here'],
+    [['at end->{}', 'here'], 'at end->here'],
     [['{}', null], 'null'],
     [['{}{}', null, null], 'nullnull']
   ]
 
-  for (const [args, expected] of formatTests) {
+  for (const [args, expected] of goodFormatTests) {
     it(`format(${args.map(a => '"' + a + '"').join(', ')})`, () => {
       const result = intrinsics.format(...args)
       expect(result).to.equal(expected)
     })
   }
+
+  it('format("test", "extra", "arguments") fails', () => {
+    const test = () => intrinsics.format('test', 'extra', 'arguments')
+    expect(test, "format('test', 'extra', 'arguments') should throw").to.throw()
+  })
 })
