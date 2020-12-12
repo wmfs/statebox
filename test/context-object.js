7fde89160bc93ce0f3e3be5c28a7508dc7f14544
Jez Higgins <jez@jezuk.co.uk>
2020-12-12 13:08:39 +0000

    test: Context Object handling - first few tests

---
+++ test/context-object.js
@@ -0,0 +1,49 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const expect = chai.expect
+
+const contextObjectStateMachines = require('./fixtures/state-machines/context-object')
+
+const Statebox = require('./../lib')
+
+let statebox
+
+describe('Context Object', () => {
+  before('setup statebox', async () => {
+    statebox = new Statebox()
+    await statebox.ready
+    await statebox.createStateMachines(contextObjectStateMachines, {})
+  })
+
+  const today = new Date().toLocaleDateString('en-EN', { weekday: 'long' })
+
+  const contextObjectStates = {
+    NonExistantProperty: { oops: null },
+    DayOfWeek: { day: today },
+    FormattedDayOfWeek: { day: `The day is ${today}` }
+  }
+
+  for (const [name, result] of Object.entries(contextObjectStates)) {
+    test(name, result)
+  }
+})
+
+function test (statemachine, result) {
+  it(statemachine, async () => {
+    const executionDescription = await runStateMachine(statemachine)
+
+    expect(executionDescription.status).to.eql('SUCCEEDED')
+    expect(executionDescription.stateMachineName).to.eql(statemachine)
+    expect(executionDescription.currentResource).to.eql(undefined)
+    expect(executionDescription.ctx).to.eql(result)
+  }) // it ...
+}
+
+function runStateMachine (statemachine) {
+  return statebox.startExecution(
+    {}, // input
+    statemachine,
+    { sendResponse: 'COMPLETE' } // options
+  )
+}
---
+++ test/fixtures/state-machines/context-object/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NonExistantProperty: require('./built-ins/non-existant.json'),
+  DayOfWeek: require('./built-ins/day-of-week.json'),
+  FormattedDayOfWeek: require('./built-ins/formatted-day-of-week.json')
+}
